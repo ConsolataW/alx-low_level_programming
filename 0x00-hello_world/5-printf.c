fd637684d543409eedacdd9992be6b01751f92ad
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-02-16 13:54:03 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,8 @@
 /**
  * main - we use printf in this program to output
  * Return: 0 success
- * /
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
+return (0);}
