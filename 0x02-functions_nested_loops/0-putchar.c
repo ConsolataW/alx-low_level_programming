272688edbbafbaed413b7e952a04cb891442d1df
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-02-23 07:35:49 -0800

    revised 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,5 @@
-#include "main.h" 
-  
- /** 
-  * main - entry point 
-  * Return: always return 0 
-  */ 
- int main(void) 
+#include "main.h"
+int main(void)
  { 
  putchar(''); 
  _putchar('p'); 
