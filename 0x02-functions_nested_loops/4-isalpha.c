033feb22b0d15a5f76f149e45813a53dd6a7704b
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-02-27 13:41:09 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,11 @@
-#include "main.h" 
-  
- /** 
-  * _isalpha - main function 
-  * @c: c is an ascii character 
-  * Return: 1 if true, 0 if false 
-  */ 
- int _isalpha(int c) 
- { 
- if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) 
- { 
- return (1); 
- } 
- return (0); 
- }
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
