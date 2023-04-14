fc4b7131763d6f6e04d7a7d8f5ad992b30dc0849
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-04-14 14:47:03 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,18 +3,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
- */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-	if (!filename)
+	int fd, nrd, nwr;
+	char *buf;
+
+	if (!filename || letters == 0)
 	return (0);
 
 	int fd = open(filename, O_RDONLY);
@@ -31,15 +26,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	return (0);
 	}
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-	return (0);
-
 	nrd = read(fd, buf, letters);
+
+	if (nrd == -1)
+	{
+	close(fd);
+	free(buf);
+	return (0);
+	}
 	nwr = write(STDOUT_FILENO, buf, nrd);
 
+	if (nwr == -1 || nwr != nrd)
+	{
 	close(fd);
+	free(buf);
+	return (0);
+	}
 
+	close(fd);
 	free(buf);
 
 	return (nwr);
