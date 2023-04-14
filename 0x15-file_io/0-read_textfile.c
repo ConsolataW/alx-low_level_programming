b93bd5b7158920ff94d6c1d16903092dfc66b123
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-04-14 14:58:03 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,45 +8,29 @@
  * Return: numbers of letters printed. If it fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
+
 {
-	int fd, nrd, nwr;
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
 
-	if (!filename || letters == 0)
-	return (0);
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
-	return (0);
+		return (0);
 
-	buf = malloc(sizeof(char) * (letters + 1));
-
-	if (buf == NULL)
-	{
-	close(fd);
-	return (0);
-	}
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
 	nrd = read(fd, buf, letters);
-
-	if (nrd == -1)
-	{
-	close(fd);
-	free(buf);
-	return (0);
-	}
-
 	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	if (nwr == -1 || nwr != nrd)
-	{
 	close(fd);
-	free(buf);
-	return (0);
-	}
 
-	close(fd);
 	free(buf);
 
 	return (nwr);
