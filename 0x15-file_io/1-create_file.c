aa64d647f52c878fc43b4029629888d1887fc0d7
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-04-15 12:04:24 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * create_file - function entry
- * Description: create a file
- * @param *filename
- * @param text_content
+ * create_file - creates a file and writes text to it
+ *
+ * @filename: the name of the file to create
+ * @text_content: the text to write to the file
+ *
  * @return 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
