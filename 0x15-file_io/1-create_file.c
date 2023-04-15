fb5da208d79badd57230fffb9379d706ffb0d306
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-04-15 12:10:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,15 +16,19 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (!fd)
+	if (fd == -1)
 	return (-1);
 
 	if (text_content == NULL)
 	text_content = "";
 	size = strlen(text_content);
 	wrn = write(fd, text_content, size);
-	if (!wrn)
+	if (wrn == -1)
+	{
+
+	close(fd);
 	return (-1);
+	}
 
 	close(fd);
 	return (1);
