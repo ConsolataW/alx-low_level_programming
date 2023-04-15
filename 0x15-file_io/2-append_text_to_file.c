13249fff7556f02eef1c518f6a98703dfc55a210
ConsolataW <wairimuconsolata.cw@gmail.com>
2023-04-15 13:15:23 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	return (-1);
 
 	size = strlen(text_content);
-	size wrn = write(fd, text_content, size);
+	wrn = write(fd, text_content, size);
 
-	{
 	if (wrn == -1)
-	return -1;
+	return (-1);
+
+	close (fd);
+	return (-1);
 }
